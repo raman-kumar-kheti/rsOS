1d03caf0cba7798551e781b1e7189243b22de80d
Raman Kumar Kheti <ramank.kheti@gmail.com>
2025-04-15 18:20:00 +0530

    added a splash screen

---
+++ config/includes.chroot/opt/bootScreen/boot_screen.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from PyQt6.QtWidgets import (QWidget, QApplication, QVBoxLayout, QLabel, QHBoxLayout)
+from PyQt6.QtCore import Qt, QSize, QRect
+from PyQt6.QtGui import QMovie, QPixmap
+import os
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class SplashScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(Qt.WindowType.Window | Qt.WindowType.CustomizeWindowHint)
+        self.setStyleSheet("background: black;")
+        self.setGeometry(0, 0, 1920, 1080)
+
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignBottom)
+
+        self.os_logo = QLabel()
+        self.os_logo.setPixmap(QPixmap(os.path.join(SCRIPT_DIR,"os_logo.png")).scaled(200, 200, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        self.os_logo.setFixedSize(200, 200)
+        self.os_logo.setScaledContents(True)
+
+        self.spinner_mover = QMovie(os.path.join(SCRIPT_DIR, "initial_spinner.gif"))
+        self.spinner_mover.start()
+        self.spinner_mover.setScaledSize(QSize(25, 25))
+
+        self.spinner_holder = QLabel(self)
+        self.spinner_holder.setMovie(self.spinner_mover)
+        self.spinner_holder.setVisible(True)
+        self.spinner_holder.setFixedSize(25, 25)
+
+        self.bottom_heading = QLabel()
+        self.bottom_heading.setStyleSheet("""
+            QLabel{
+                font-size:40px;
+                margin-bottom:100px;
+                margin-top:20px;
+                font-family: "Times New Roman", Times, serif;
+            }
+        """)
+        self.bottom_heading.setText("Fllamingo.")
+
+        middle_container = QVBoxLayout()
+        middle_container.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        middle_container.setSpacing(180)
+        middle_container.addWidget(self.os_logo)
+        middle_container.addWidget(self.spinner_holder, alignment=Qt.AlignmentFlag.AlignHCenter)
+
+
+        self.main_layout.addLayout(middle_container)
+        self.main_layout.addWidget(self.bottom_heading)
+
+if __name__ == "__main__":
+    splash_screen = QApplication([])
+
+    splash_screen_View = SplashScreen()
+    splash_screen_View.showFullScreen()
+
+    splash_screen.exec()
