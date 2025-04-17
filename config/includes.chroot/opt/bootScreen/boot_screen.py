5c286714cb2191da463849d203d21d49fe1bd371
Raman Kumar Kheti <ramank.kheti@gmail.com>
2025-04-17 12:15:23 +0530

    updated the splash screen logic

--- config/includes.chroot/opt/bootScreen/boot_screen.py
+++ config/includes.chroot/opt/bootScreen/boot_screen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from PyQt6.QtWidgets import (QWidget, QApplication, QVBoxLayout, QLabel, QHBoxLayout)
-from PyQt6.QtCore import Qt, QSize, QRect
-from PyQt6.QtGui import QMovie, QPixmap
-import os
+from PyQt6.QtWidgets import (QWidget, QApplication, QVBoxLayout, QLabel, QPushButton)
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QMovie, QPixmap, QIcon
+import os, sys
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class SplashScreen(QWidget):
@@ -16,6 +16,11 @@ class SplashScreen(QWidget):
 
         self.main_layout = QVBoxLayout(self)
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignBottom)
+        bottomBtn = QPushButton(
+            text="Bottom",
+            parent=self
+        )
+        bottomBtn.clicked.connect(self.closeSplashScreen)
 
         self.os_logo = QLabel()
         self.os_logo.setPixmap(QPixmap(os.path.join(SCRIPT_DIR,"os_logo.png")).scaled(200, 200, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
@@ -38,6 +43,7 @@ class SplashScreen(QWidget):
                 margin-bottom:100px;
                 margin-top:20px;
                 font-family: "Times New Roman", Times, serif;
+                color:white;
             }
         """)
         self.bottom_heading.setText("Fllamingo.")
@@ -45,6 +51,7 @@ class SplashScreen(QWidget):
         middle_container = QVBoxLayout()
         middle_container.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         middle_container.setSpacing(180)
+        middle_container.addWidget(bottomBtn)
         middle_container.addWidget(self.os_logo)
         middle_container.addWidget(self.spinner_holder, alignment=Qt.AlignmentFlag.AlignHCenter)
 
@@ -52,6 +59,10 @@ class SplashScreen(QWidget):
         self.main_layout.addLayout(middle_container)
         self.main_layout.addWidget(self.bottom_heading)
 
+    def closeSplashScreen(self):
+            print("Is This method called")
+            self.close()
+            sys.exit(0)
 if __name__ == "__main__":
     splash_screen = QApplication([])
 
---
+++ config/includes.chroot/usr/local/bin/splash-screen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISPLAY=:0
+openbox-session & /opt/bootScreen/boot_screen.py
